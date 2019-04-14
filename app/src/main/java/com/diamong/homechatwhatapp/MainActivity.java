df7e955451584e46f12cfc29afa46d985c561e23
diamong01 <45653307+diamong01@users.noreply.github.com>
2019-04-14 15:11:28 +0900

    상단 TabBar 강의5완료
    잘해봅시다.

--- app/src/main/java/com/diamong/homechatwhatapp/MainActivity.java
+++ app/src/main/java/com/diamong/homechatwhatapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.diamong.homechatwhatapp;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -7,6 +9,10 @@ import android.support.v7.widget.Toolbar;
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
+    private ViewPager myViewPager;
+    private TabLayout myTabLayout;
+    private TabsAccessorAdapter myTabsAccessorAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +23,13 @@ public class MainActivity extends AppCompatActivity {
         mToolbar=findViewById(R.id.main_page_toolbar);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle("My App");
+
+        myViewPager= findViewById(R.id.main_tabs_pager);
+        myTabsAccessorAdapter = new TabsAccessorAdapter(getSupportFragmentManager());
+        myViewPager.setAdapter(myTabsAccessorAdapter);
+
+        myTabLayout=findViewById(R.id.main_tabs);
+        myTabLayout.setupWithViewPager(myViewPager);
 
     }
 }
